7fa2c66c26d3f859fa496bc92dd524d0f1f2fc7f
이정민 <whljm1003@gmail.com>
2021-12-30 20:36:35 +0900

    fisrt steps with typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "JM", age = 24, gender = "man";
+const sayHi = (name, age, gende) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "JM",
+  age = 24,
+  gender = "man";
+
+const sayHi = (name, age, gende?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
