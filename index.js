d4e09d3d36ffc55c6eb3c00fed86df423836a76f
이정민 <whljm1003@gmail.com>
2021-12-30 20:58:53 +0900

    Types in Typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "JM", age = 24, gender = "man";
-const sayHi = (name, age, gende) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
