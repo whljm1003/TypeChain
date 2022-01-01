da713f80c21ed7c7d24cbe87b3eadc87de54e21b
이정민 <whljm1003@gmail.com>
2022-01-01 15:24:55 +0900

    Class on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
+class Human {
   name: string;
   age: number;
   gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "jm",
-  age: 24,
-  gender: "man",
-};
+const JM = new Human("jungmin", 24, "man");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(JM));
 
 export {};
